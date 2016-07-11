c49d92566bf8804433ae94f4abc60c3ec0be32db
lvlin <1183945397@qq.com>
2016-07-11 10:20:02 +0800

    创建工程

---
+++ Test01/app/src/main/java/com/lv/dllo/test01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lv.dllo.test01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
